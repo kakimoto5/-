7074ae525fa3bab2505e8404170ab1ed7cea046c
kakimoto <75213496+kakimoto5@users.noreply.github.com>
2020-12-15 22:03:29 +0900

    ローディング画面
    setTimeout
    classList.remove
     opacity: 0;
      visibility: hidden;
      transition: 0.3s;

---
+++ script.js
@@ -0,0 +1,9 @@
+function loaded () {
+  document.getElementById("loading").classList.remove("active");
+}
+
+window.addEventListener("load", function() {
+  setTimeout(loaded, 500)
+})
+
+setTimeout(loaded, 5000)
